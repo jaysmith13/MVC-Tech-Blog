f849c5e9c99c195037f04a92faef0bc5fbf19915
jaysmith13 <janeasmith13@gmail.com>
2022-08-28 02:13:36 -0600

    update
    error fixes and function updates

--- models/user.js
+++ models/user.js
@@ -2,7 +2,58 @@ const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+class User extends Model {
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
 
+// Model functions
+User.init(
+  {
+
+    id: {
+        
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull:false,
+        unique: true
+    }, 
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [4]
+        }
+    }
+  },
+  {
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+            
+        },
+        // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
+    
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
+  }
+);
 
 
 
