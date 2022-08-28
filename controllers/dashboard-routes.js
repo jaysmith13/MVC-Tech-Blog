0a1c60c28c43c0f0be1cb8ff975b6252ac270676
jaysmith13 <janeasmith13@gmail.com>
2022-08-28 01:30:12 -0600

    update
    adding routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,10 +1,7 @@
+const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
-const router = require('express').Router();
-
-
-
-
+const withAuth = require('../utils/auth');
 
 
 
