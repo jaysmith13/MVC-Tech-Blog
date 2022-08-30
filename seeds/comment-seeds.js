f68f85422916e5b25324688b9be4747c30c121f0
jaysmith13 <janeasmith13@gmail.com>
2022-08-30 00:58:57 -0600

    update
    updates and error fixes

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -1,17 +1,17 @@
 const { Comment } = require('../models');
 
 const commentData = [{
-        comment_text: "Lorem ipsum dolor sit amet",
+        comment_text: "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.",
         user_id: 1,
         post_id: 1
     },
     {
-        comment_text: "consectetur adipiscing elit",
+        comment_text: "Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh",
         user_id: 2,
         post_id: 2
     },
     {
-        comment_text: "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        comment_text: "Proin eu mi. Nulla ac enim",
         user_id: 3,
         post_id: 3
     }
