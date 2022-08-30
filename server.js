1796483ba662ee4271e97dc0a3893d25498b26f8
jaysmith13 <janeasmith13@gmail.com>
2022-08-30 01:35:08 -0600

    ugh
    got rid of one error now have a different one about the db

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,6 +26,8 @@ const sess = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
